58596eb73266b32c204c6005786610564893f248
Mateusz <wszeborowskimateusz@gmail.com>
2017-09-10 12:04:10 +0200

    Addind numbers words to NumberActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,8 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -39,5 +41,13 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("eight");
         words.add("nine");
         words.add("ten");
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+        for (int index = 0; index < words.size(); index++){
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+        }
     }
 }
