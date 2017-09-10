428f6f27e9d605d6d16128e5a4fd59e8f8b5232f
Mateusz <wszeborowskimateusz@gmail.com>
2017-09-10 18:42:57 +0200

    Deleting setting status bar color in NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -16,7 +16,6 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
-import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
@@ -57,7 +56,6 @@ public class NumbersActivity extends AppCompatActivity {
         // word_list.xml file.
         ListView listView = (ListView) findViewById(R.id.list);
 
-        listView.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.category_numbers, null));
         // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
         // {@link ListView} will display list items for each word in the list of words.
         // Do this by calling the setAdapter method on the {@link ListView} object and pass in
